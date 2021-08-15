99a1a1d214f589fef4ca3595cc6098e4d2bb9ece
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-08-15 15:40:42 +0300

    done

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+
+const notFoundHandler= require('./handlers/404.js');
+const errorHandler = require('./handlers/500.js');
+
+app.get('/' , (req,res)=>{
+ 
+    res.status(200).send('Hello Mahmoud');
+});
+
+
+app.get('/data' ,(req,res)=>{
+    let output = {
+        10:'even',
+        5:'odd',
+        time : new Date().toString()
+    }
+    res.status(200).json(output);
+});
+
+app.get('/bad' , (req,res,next)=>{
+    next('error from bad end point');
+});
+
+app.use('*',notFoundHandler);
+app.use(errorHandler);
+
+function start(port) {
+    app.listen(port,()=> console.log(`Server started on port ${port}`))
+    
+}
+
+module.exports = {
+    start,
+    app
+}
